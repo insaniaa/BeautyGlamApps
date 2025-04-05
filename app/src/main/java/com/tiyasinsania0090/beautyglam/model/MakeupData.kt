03448f74693c3fc167f72777f74baf5cad50f96b
Tiyas Insania <tiyasinsania@gmail.com>
2025-04-05 22:55:44 +0700

    Update Makeup Data

--- app/src/main/java/com/tiyasinsania0090/beautyglam/model/MakeupData.kt
+++ app/src/main/java/com/tiyasinsania0090/beautyglam/model/MakeupData.kt
@@ -1,144 +1,17 @@
 package com.tiyasinsania0090.beautyglam.model
 
 val makeupStyles = listOf(
-    MakeupStyle(
-        name = "Glam Girl Makeup",
-        skinType = listOf("Combination"),
-        skinTone = listOf("Medium", "Dark"),
-        undertone = listOf("Warm"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Grunge Makeup",
-        skinType = listOf("Oily", "Combination"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Cool"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Latina Makeup",
-        skinType = listOf("Combination"),
-        skinTone = listOf("Medium", "Dark"),
-        undertone = listOf("Warm"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Clean Makeup",
-        skinType = listOf("Normal", "Combination"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Igari Makeup",
-        skinType = listOf("Dry"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Cool"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Douyin Makeup",
-        skinType = listOf("Normal"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Ulzzang Makeup",
-        skinType = listOf("Normal"),
-        skinTone = listOf("Fair"),
-        undertone = listOf("Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Bayonetta Makeup",
-        skinType = listOf("Oily", "Combination"),
-        skinTone = listOf("Medium", "Dark"),
-        undertone = listOf("Cool"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Gyaru Makeup",
-        skinType = listOf("Oily"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Warm"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Idol Makeup",
-        skinType = listOf("Normal"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Doll Makeup",
-        skinType = listOf("Dry"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Latte Makeup",
-        skinType = listOf("Dry", "Normal"),
-        skinTone = listOf("Medium", "Dark"),
-        undertone = listOf("Warm", "Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Strawberry Makeup",
-        skinType = listOf("Dry"),
-        skinTone = listOf("Fair"),
-        undertone = listOf("Cool"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Cinnamon Girl Makeup",
-        skinType = listOf("Dry", "Normal"),
-        skinTone = listOf("Medium"),
-        undertone = listOf("Warm"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Dark Feminine Makeup",
-        skinType = listOf("Normal"),
-        skinTone = listOf("Dark"),
-        undertone = listOf("Cool"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Arabian Makeup",
-        skinType = listOf("Combination"),
-        skinTone = listOf("Medium", "Dark"),
-        undertone = listOf("Warm"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Smokie Makeup",
-        skinType = listOf("Oily"),
-        skinTone = listOf("Dark"),
-        undertone = listOf("Neutral", "Cool"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Tomie Makeup",
-        skinType = listOf("Combination"),
-        skinTone = listOf("Medium", "Dark"),
-        undertone = listOf("Cool"),
-        visualType = "High"
-    ),
-    MakeupStyle(
-        name = "Natural Makeup",
-        skinType = listOf("Normal"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Neutral"),
-        visualType = "Low"
-    ),
-    MakeupStyle(
-        name = "Y2K Makeup",
-        skinType = listOf("Oily"),
-        skinTone = listOf("Fair", "Medium"),
-        undertone = listOf("Neutral"),
-        visualType = "High"
-    )
+    MakeupStyle("Combination", "Medium", "Warm", "High", "Glam Girl Makeup"),
+    MakeupStyle("Oily", "Fair", "Cool", "High", "Grunge Makeup"),
+    MakeupStyle("Combination", "Medium", "Warm", "High", "Latina Makeup"),
+    MakeupStyle("Normal", "Fair", "Neutral", "Low", "Clean Makeup"),
+    MakeupStyle("Dry", "Fair", "Cool", "High", "Igari Makeup"),
+    MakeupStyle("Normal", "Fair", "Neutral", "Low", "Douyin Makeup"),
+    MakeupStyle("Normal", "Fair", "Neutral", "Low", "Ulzzang Makeup"),
+    MakeupStyle("Oily", "Medium", "Cool", "High", "Bayonetta Makeup"),
+    MakeupStyle("Oily", "Fair", "Warm", "High", "Gyaru Makeup"),
+    MakeupStyle("Normal", "Fair", "Neutral", "Low", "Idol Makeup"),
+    MakeupStyle("Dry", "Fair", "Neutral", "Low", "Doll Makeup"),
+    MakeupStyle("Normal", "Medium", "Warm", "Low", "Cinnamon Girl Makeup"),
+    MakeupStyle("Combination", "Dark", "Cool", "High", "Tomie Makeup")
 )
--- app/src/main/java/com/tiyasinsania0090/beautyglam/model/MakeupStyle.kt
+++ app/src/main/java/com/tiyasinsania0090/beautyglam/model/MakeupStyle.kt
@@ -1,9 +1,9 @@
 package com.tiyasinsania0090.beautyglam.model
 
 data class MakeupStyle(
-    val name: String,
-    val skinType: List<String>, // Oily, Dry, Normal, Combination
-    val skinTone: List<String>, // Fair, Medium, Dark
-    val undertone: List<String>, // Warm, Neutral, Cool
-    val visualType: String // Low atau High
+    val skinType: String,
+    val skinTone: String,
+    val undertone: String,
+    val visualType: String,
+    val styleName: String
 )
