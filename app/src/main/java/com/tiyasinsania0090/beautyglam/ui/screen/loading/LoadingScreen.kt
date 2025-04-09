4b653594597afb6561cd09ea0e9cfe0a6d539d44
Tiyas Insania <tiyasinsania@gmail.com>
2025-04-10 02:44:39 +0700

    membuat loading awal

--- app/src/main/java/com/tiyasinsania0090/beautyglam/ui/screen/loading/LoadingScreen.kt
+++ app/src/main/java/com/tiyasinsania0090/beautyglam/ui/screen/loading/LoadingScreen.kt
@@ -14,27 +14,24 @@ import kotlinx.coroutines.delay
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun SplashScreen(navController: NavController) {
+
     var showLogo by remember { mutableStateOf(true) }
     var dotCount by remember { mutableIntStateOf(1) }
 
     LaunchedEffect(Unit) {
-        delay(2000L) // Tampilkan logo 2 detik
+        delay(2000L) // Delay before showing loading animation
         showLogo = false
 
-        // Mulai animasi loading setelah logo
-        repeat(6) { // Total: 3 detik (6 x 500ms)
+        repeat(6) {
             delay(500)
             dotCount = (dotCount % 3) + 1
         }
 
-        // Navigasi ke Home
-        navController.navigate("home") {
-            popUpTo("splash") { inclusive = true } // biar splash ga bisa di-back
+        navController.navigate("input") {
+            popUpTo("loading") { inclusive = true }
         }
     }
 
@@ -46,22 +43,20 @@ fun SplashScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         if (showLogo) {
-            // Logo App (2 detik pertama)
             Image(
                 painter = painterResource(id = R.drawable.logobeautyglam),
                 contentDescription = "Logo App",
                 modifier = Modifier
-                    .height(150.dp)
-                    .width(150.dp)
+                    .height(270.dp)
+                    .width(270.dp)
             )
         } else {
-            // Loading Bunny
             Image(
                 painter = painterResource(id = R.drawable.loadingbunny),
                 contentDescription = "Loading Bunny",
                 modifier = Modifier
-                    .height(121.dp)
-                    .width(121.dp)
+                    .height(120.dp)
+                    .width(120.dp)
             )
             Spacer(modifier = Modifier.height(20.dp))
             Text(
@@ -72,10 +67,4 @@ fun SplashScreen(navController: NavController) {
             )
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SplashScreenPreview() {
-    SplashScreen(navController = rememberNavController())
 }
