f023cc37cb66701c95b18b2dfc142fb7600f093e
Tiyas Insania <tiyasinsania@gmail.com>
2025-04-10 15:54:08 +0700

    Fixing Navigasi Aplikasi

--- app/src/main/java/com/tiyasinsania0090/beautyglam/Navigation/AppNavigation.kt
+++ app/src/main/java/com/tiyasinsania0090/beautyglam/Navigation/AppNavigation.kt
@@ -1,49 +1,73 @@
 package com.tiyasinsania0090.beautyglam.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tiyasinsania0090.beautyglam.ui.screen.loading.SplashScreen
+import androidx.navigation.NavType
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.tiyasinsania0090.beautyglam.data.getRecommendedMakeup
+import com.tiyasinsania0090.beautyglam.ui.screen.about.AboutScreen
 import com.tiyasinsania0090.beautyglam.ui.screen.input.InputScreen
 import com.tiyasinsania0090.beautyglam.ui.screen.result.ResultScreen
-import com.tiyasinsania0090.beautyglam.ui.screen.about.AboutScreen
+import com.tiyasinsania0090.beautyglam.ui.screen.loading.SplashScreen
 
 @Composable
-fun AppNavigation(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = "loading"
-    ) {
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "loading") {
         composable("loading") {
-            SplashScreen(navController = navController) // Navigating to InputScreen
+            SplashScreen(navController = navController)
         }
+
         composable("input") {
             InputScreen(
-                onSubmit = { name, skinType, skinTone, undertone, visualType ->
-                    // After input, navigate to result screen
-                    navController.navigate("result")
+                onSubmit = { name, skinType, skinTone, undertone, visualType, makeupUses ->
+                    navController.navigate("result/$name/$skinType/$skinTone/$undertone/$visualType/$makeupUses")
                 },
                 onNavigateAbout = {
-                    // Navigate to About Screen
                     navController.navigate("about")
                 }
             )
         }
-        composable("result") {
+
+        composable(
+            "result/{name}/{skinType}/{skinTone}/{undertone}/{visualType}/{makeupUses}",
+            arguments = listOf(
+                navArgument("name") { type = NavType.StringType },
+                navArgument("skinType") { type = NavType.StringType },
+                navArgument("skinTone") { type = NavType.StringType },
+                navArgument("undertone") { type = NavType.StringType },
+                navArgument("visualType") { type = NavType.StringType },
+                navArgument("makeupUses") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val name = backStackEntry.arguments?.getString("name") ?: ""
+            val skinType = backStackEntry.arguments?.getString("skinType") ?: ""
+            val skinTone = backStackEntry.arguments?.getString("skinTone") ?: ""
+            val undertone = backStackEntry.arguments?.getString("undertone") ?: ""
+            val visualType = backStackEntry.arguments?.getString("visualType") ?: ""
+            val makeupUses = backStackEntry.arguments?.getString("makeupUses") ?: ""
+
+            val recommended =
+                getRecommendedMakeup(skinType, skinTone, undertone, visualType, makeupUses)
+
             ResultScreen(
-                userName = "User", // Customize to pass actual user data
-                makeupStyle = com.tiyasinsania0090.beautyglam.data.MakeupStyle("MakeupStyle", emptyList(), emptyList(), emptyList(), emptyList(), emptyList(), "imageName"),
-                onNext = {
-                    navController.navigate("input") // Navigate back to input after result
-                },
+                userName = name,
+                recommendedStyles = recommended,
                 onBack = {
-                    navController.popBackStack() // Navigate back to previous screen
+                    navController.popBackStack()
                 }
             )
         }
+
         composable("about") {
-            AboutScreen(navController = navController) // Navigate to About Screen
+            AboutScreen(
+                onBack = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
